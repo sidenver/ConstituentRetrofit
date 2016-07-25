8fe1c8a23218e534bc91a6c05f8db05b29280a13
sidenver <hanchinshing@gmail.com>
2016-07-25 10:44:30 -0400

    random shuffle documents input

--- trainWord2VecUncompress.py
+++ trainWord2VecUncompress.py
@@ -53,7 +53,8 @@ class MySentences(object):
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    myDirList = np.random.shuffle(os.listdir(dataPath))
+    myDirList = os.listdir(dataPath)
+    np.random.shuffle(myDirList)
     sentences = MySentences(dataPath, phraseDir, concatenate=False, dirList=myDirList)  # a memory-friendly iterator
     model = gensim.models.Word2Vec(sentences, size=300, min_count=5, window=5, workers=multiprocessing.cpu_count())
     model.save(savePath + 'wordOnly2')
